2f77772ec2696b684fa119619bc2e7c8fab02bc6
Simon Rondelez <simonrondelez@me.com>
2019-03-21 09:25:35 +0100

    Add monitoring for api-gateway

---
+++ api_gateway/main.tf
@@ -0,0 +1,59 @@
+// Setup Cloudwatch alarms for passed API gateway
+// Send alerts to given SNS topics.
+
+resource "aws_cloudwatch_metric_alarm" "ApiGateway_5XXError" {
+  alarm_name          = "${var.api_gateway}_5XXError"
+  alarm_description   = "The errors on ${var.api_gateway} are higher than ${var.5XXError_treshold} for ${var.5XXError_period}"
+  namespace           = "AWS/ApiGateway"
+  metric_name         = "5XXError"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "${var.5XXError_treshold}"
+  evaluation_periods  = "${var.5XXError_evaluation_periods}"
+  period              = "${var.5XXError_period}"
+
+  alarm_actions = ["${var.sns_topic_arn}"]
+  ok_actions    = ["${var.sns_topic_arn}"]
+
+  dimensions {
+    FunctionName = "${var.api_gateway}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "ApiGateway_latency" {
+  alarm_name          = "${var.api_gateway}_latency"
+  alarm_description   = "The latency on ${var.api_gateway} is higher than ${var.latency_treshold} for ${var.latency_period}"
+  namespace           = "AWS/ApiGateway"
+  metric_name         = "Latency"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "${var.latency_treshold}"
+  evaluation_periods  = "${var.latency_evaluation_periods}"
+  period              = "${var.latency_period}"
+
+  alarm_actions = ["${var.sns_topic_arn}"]
+  ok_actions    = ["${var.sns_topic_arn}"]
+
+  dimensions {
+    FunctionName = "${var.api_gateway}"
+  }
+}
+
+resource "aws_cloudwatch_metric_alarm" "ApiGateway_integrationlatency" {
+  alarm_name          = "${var.api_gateway}_integrationlatency"
+  alarm_description   = "The latency on ${var.api_gateway} is higher than ${var.integrationlatency_treshold} for ${var.integrationlatency_period}"
+  namespace           = "AWS/ApiGateway"
+  metric_name         = "IntegrationLatency"
+  statistic           = "Sum"
+  comparison_operator = "GreaterThanThreshold"
+  threshold           = "${var.integrationlatency_treshold}"
+  evaluation_periods  = "${var.integrationlatency_evaluation_periods}"
+  period              = "${var.integrationlatency_period}"
+
+  alarm_actions = ["${var.sns_topic_arn}"]
+  ok_actions    = ["${var.sns_topic_arn}"]
+
+  dimensions {
+    FunctionName = "${var.api_gateway}"
+  }
+}
